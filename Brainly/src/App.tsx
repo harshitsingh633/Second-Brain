9782c06da38c04af8ce9468dbf5b7e709debaf4a
Harshit singh <hs8700882@gmail.com>
2025-03-23 14:29:07 +0530

    Creating Button.tsx

--- Brainly/src/App.tsx
+++ Brainly/src/App.tsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from './Components/ui/Button'
 
 function App() {
 return <div>
-    
+    <Button variant='primary' text='Share' size='sm' />
+    <Button variant='secondary' text='Add Content' size='sm' />
+
 </div>
  
 }
--- Brainly/src/Components/ui/Button.tsx
+++ Brainly/src/Components/ui/Button.tsx
@@ -1,14 +1,34 @@
+import { ReactElement } from "react";
 
 interface ButtonProps{
     variant : "primary" | "secondary";
     size : "sm" |"md" | "lg";
     text: string;
-    startIcon?: any;
-    endIcon?: any;
+    startIcon?: ReactElement;
+    endIcon?: ReactElement;
     onClick : () => void;
 }
 
-export const Button = (props: ButtonProps) =>{
-    return <button></button>
+const variantStyle = {
+    "primary": "bg-purple-600 text-white",
+    "secondary": "bg-purple-400 text-purple-600"
 }
-<Button variant="primary" size="md" text={"asd"} onClick={()=> {}}></Button>
+
+const sizeStyle = {
+    "sm " : "p-6",
+    "md" : "p-4",
+    "lg" : "p-6"
+}
+const defaultStyle = "rounded-md"
+
+export const Button = (props: ButtonProps) =>{
+    return <button className={`${variantStyle[props.variant]} ${defaultStyle} ${sizeStyle[props.size]} `}>{props.text} </button>
+}
+{/* <Button variant="primary" size="md" text={"asd"} onClick={()=> {}}></Button> */}
+
+
+//#e0e7fe
+//#5046e4
+//#3e38a7
+
+//54.27
