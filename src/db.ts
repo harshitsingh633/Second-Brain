419982349c89495f50a60c3b0d01326c7df50778
Harshit singh <hs8700882@gmail.com>
2025-03-26 11:10:55 +0530

    Updated Routes

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,12 @@
-import mongoose ,{ model, Schema } from "mongoose";
 import * as dotenv from "dotenv";
+dotenv.config();
+import mongoose ,{ model, Schema } from "mongoose";
 
-mongoose.connect(``);
+if(!process.env.MONGO_URL){
+      throw new Error ("MONGO_URL is not present in the Environment Variable file");
+}
+
+mongoose.connect(`${process.env.MONGO_URL}`);
 
 const UserSchema = new Schema({
       username : {type: String , unique : true},
--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,83 @@
-import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import jwt from "jsonwebtoken";
 import { UserModel } from "./db";
-const Jwt_password = process.env.Jwt_password;
-
+import bcrypt from "bcrypt";
+import { z } from "zod";
 const app = express();
-app.use(express.json());
-app.post("/api/v1/signup", async(req, res) => {
- // TODO: zod validation , hash the password
-  const {username , password} = req.body;
 
-  await UserModel.create({
-    username : username,
-    password : password
-  })
-  res.json({
-    message: "User signed up"
-  })
+
+if(!process.env.Jwt_Password){
+  throw new Error("JWT_SECRET is not set in the environment variables");
+}
+
+const Jwt_password = process.env.Jwt_Password;
+
+app.use(express.json());
+
+const UserSchema = z.object({
+    username : z.string().min(3).max(20),
+    password : z.string().min(6).max(50),
+})
+
+app.post("/api/v1/signup", async (req, res) => {
+  // TODO: zod validation , hash the password
+  const { username, password } = UserSchema.parse(req.body);
+
+  const hashPassword = await bcrypt.hash(password,10);
+
+  try {
+    await UserModel.create({
+      username: username,
+      password: hashPassword,
+    });
+    res.json({
+      message: "User signed up",
+    });
+  } catch (e) {
+    res.status(411).json({
+      message: "User already exsists",
+    });
+  }
 });
 
-app.post("/api/v1/signin",async (req, res) => {
-    const {username , password} = req.body;
-    const existingUser = await UserModel.findOne({
-      username,
-      password
-    })
-    if(existingUser){
-      const token = jwt.sign({
-        id : existingUser._id
-      },Jwt_password)
 
-      res.json({
-        token
-      })
-    }else{
-      res.status(403).json({
-        message: "Incorrect credentials"
-      })
-    }
+
+app.post("/api/v1/signin", async (req, res) => {
+  const { username, password } = UserSchema.parse(req.body);
+
+
+  const existingUser = await UserModel.findOne({
+    username
+  });
+  if (existingUser) {
+    const token = jwt.sign(
+      {
+        id: existingUser._id,
+      },
+      Jwt_password
+    );
+
+    res.json({
+      token,
+    });
+  } else {
+    res.status(403).json({
+      message: "Incorrect credentials",
+    });
+  }
 });
 
 app.post("/api/v1/content", (req, res) => {
+  const {username , password } = UserSchema.
 
 });
 
-app.get("/api/v1/content", (req, res) => {
+app.get("/api/v1/content", (req, res) => {});
 
-});
+app.delete("/api/v1/content", (req, res) => {});
 
-app.delete("/api/v1/content", (req, res) => {
+app.get("/api/v1/brain/:shareLink", (req, res) => {});
 
-});
-
-app.get("/api/v1/brain/:shareLink", (req, res) => {
-    
-});
 app.listen(8000);
